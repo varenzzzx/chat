5c7ad7145ec90bfeb017b55dc9316e50fde26b9d
varenzzzx <77892555+varenzzzx@users.noreply.github.com>
2021-01-26 20:12:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,7 @@
-var canvas : document.getElementById('canvas');
-var context= canvas.getContext('2d');
+const nama = prompt('Siapa nama mu?');
+// feedback
+alert('Nama Mu');
+alert(nama);
+alert('kan????')
 
-var radius= 10;
-var dragging = false;
-
-canvas.width= window.innerWidth;
-canvas.height= window.innerHeight;
-
-var putPoint = function(e){
-    if(dragging) {
-       context.beginPath();
-      context.arc(e.clientX, e.clientY, radius, 0, Math.PI*2);
-      context.fill();
-    }
-}
-
-var engage = function(){
-  dragging = true;
-}
-
-var disengage = function(){
-  dragging = false;
-}
-canvas.addEventListener("mousedown", engage);
-canvas.addEventListener("mousemove", putPoint);
-canvas.addEventListener("mouseup", disengage);
+const gatau = prompt('Kamu kenal aku ga??')
